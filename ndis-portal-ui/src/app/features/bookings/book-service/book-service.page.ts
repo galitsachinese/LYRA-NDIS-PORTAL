3dfbb1a987b3c6e857fb8228ca6a1e4f202e2977
Clarence <clarcabalan@gmail.com>
2026-05-14 14:48:41 +0800

    Implement book service UI

--- ndis-portal-ui/src/app/features/bookings/book-service/book-service.page.ts
+++ ndis-portal-ui/src/app/features/bookings/book-service/book-service.page.ts
@@ -4,28 +4,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { ServiceSelectComponent } from '../../../../shared/components/select/select-service/select-service.component';
-import { DatePickerComponent } from '../../../../shared/components/date-picker/date-picker.component';
-import { TextAreaComponent } from '../../../../shared/components/text-area/text-area.component';
 import { BookingService } from '../../../core/services/booking.service';
 import { ApiService } from '../../../core/services/api-service';
 
-import { BookButton } from '../../../../shared/components/button/book-button/book-button.component';
-import { BackButton } from '../../../../shared/components/button/back-button/back-button.component';
 @Component({
   selector: 'app-booking-service-page',
   standalone: true,
   imports: [
     FormsModule,
     CommonModule,
-    ServiceSelectComponent,
-    DatePickerComponent,
-    TextAreaComponent,
-    BookButton, // Added for the confirm action
-    BackButton, // Added for navigation
   ],
   templateUrl: './book-service.page.html',
-  // You can likely remove the .css file now that we are using Tailwind
+  styleUrls: ['./book-service.page.css'],
 })
 export class BookServiceComponent implements OnInit {
   bookingData = {
